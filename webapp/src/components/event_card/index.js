8c36394df5fb4396e3d7032e87b632cb303e9b94
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2018-08-12 21:09:23 +0300

    fetch events from api

--- webapp/src/components/event_card/index.js
+++ webapp/src/components/event_card/index.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
+import fetch from 'unfetch';
 import Profile from '../profile';
 
-export default ({ eventName, organizer, eventTime, eventPlace, profileImage, numOfAttendies }) => (
-  <div className="card event-card shadow bg-white rounded border-0">
-    <img className="card-img-top" src="/img/profiles/event-indoor.svg" alt="Card cap" />
-    <div className="card-body">
-      <h5 className="card-title event-name">
-        {eventName}
-      </h5>
-      <h6 className="organizer">
-        {organizer}
-      </h6>
-      <div className="datetime">
-        {eventTime} {eventPlace}
-      </div>
-      <div className="attendies">
-        <div className="profiles">
-          <Profile profileImage='./img/profiles/anon-user.svg' />
-          <Profile numOfAttendies='23' />
+export default class Events extends Component {
+  state = {
+    organizer: false,
+  }
+
+  componentDidMount() {
+    const { organizer } = this.props;
+    fetch(`https://wiggling.now.sh/company/${organizer}`)
+      .then(r => r.json())
+      .then(data => this.setState({ organizer: data }));
+  }
+
+  render() {
+    const { name, dates, place } = this.props.event;
+    const { organizer } = this.state;
+    const eventDate = new Date(dates.starts);
+    return (
+      <div className="card event-card shadow bg-white rounded border-0">
+        <img className="card-img-top" src={`/img/profiles/event-${place}.svg`} alt="host" />
+        <div className="card-body">
+          <h5 className="card-title event-name">
+            {name}
+          </h5>
+          <h6 className="organizer">
+            {organizer && organizer.name}
+          </h6>
+          <div className="datetime">
+            {eventDate.getDate()}
+            { eventDate.getMonth() + 1}
+            /
+            { eventDate.getFullYear()} &nbsp;
+            {place.toUpperCase()}
+          </div>
+          <div className="attendies">
+            <div className="profiles">
+              <Profile profileImage='./img/profiles/anon-user.svg' />
+              <Profile numOfAttendies='23' />
+            </div>
+            <span className="attending">
+              people are attending
+            </span>
+          </div>
         </div>
-        <span className="attending">
-          people are attending
-        </span>
       </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
--- webapp/src/routes/events/index.js
+++ webapp/src/routes/events/index.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import NavigationComponent from '../../components/navigation';
+import fetch from 'unfetch';
 import EventCard from '../../components/event_card';
 
-export default class Events extends Component {
-    state = {
+export default class Events extends Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      isLoading: false,
+    };
+  }
+
+    state={
+      events: false,
     }
-    render () {
-        return([
-            <NavigationComponent />,
-            <main role="main" class="container">
-                <div class="home">
-                    <div class="row">
-                        <div class="col col-lg-6 d-flex justify-content-start align-items-top pt-5">
-                            <div class="jumbotron">
-                                <h1>Upcoming Events</h1>
-                                <EventCard />
-                            </div>
-                        </div>
-                    </div>
+
+    componentDidMount() {
+      this.setState({ isLoading: true });
+
+      fetch('https://wiggling.now.sh/events')
+        .then(response => response.json())
+        .then(events => this.setState({ events, isLoading: false }));
+    }
+
+    render() {
+      const { events, isLoading } = this.state;
+      if (isLoading) {
+        return <p>Loading...</p>;
+      }
+      return (
+        <main role="main" className="container">
+          <div className="home">
+            <div className="row">
+              <div className="col col-lg-6 d-flex justify-content-start align-items-top pt-5">
+                <div className="jumbotron">
+                  <h1>Upcoming Events</h1>
+                    {events &&
+                    events.map(event=>(
+                        <EventCard key={event._id} event={event}/>
+                    ))
+                    }
                 </div>
-            </main>
-        ]
-            
-        )
+              </div>
+            </div>
+          </div>
+        </main>
+      )
     }
 }
 
